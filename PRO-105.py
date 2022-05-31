f22ff8a73f1c81f7590665041cabda10cd9c4a50
PrithviSinghNathawat <66560711+PrithviSinghNathawat@users.noreply.github.com>
2022-05-31 13:42:34 +0530

    Add files via upload

---
+++ PRO-105.py
@@ -0,0 +1,29 @@
+import csv
+with open('data3.csv') as f:
+  reader=csv.reader(f)
+  fileData=list(reader)
+data=fileData[0]
+
+def mean(data):
+  n=len(data)
+  total=0
+  for x in data:
+    total+=int(x)
+  mean=total/n
+  return mean
+squaredList=[]
+
+for number in data:
+  a=int(number)-mean(data)
+  a=a**2
+  squaredList.append(a)
+
+sum=0
+for i in squaredList:
+  sum=sum+i
+
+result=sum/(len(data)-1)
+
+import math
+sd=math.sqrt(result)
+print(sd)
